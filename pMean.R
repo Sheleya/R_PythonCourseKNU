9af540bc9a6dc5f24dc0132cade3795aba72391b
Alexander Sheleya <42179511+Sheleya@users.noreply.github.com>
2020-10-14 17:51:47 +0300

    Update pMean.R

--- pMean.R
+++ pMean.R
@@ -5,5 +5,16 @@ pmean<-function(directory, pollutant, id = 1:332) {
   for (i in id) {
     data<-rbind(data, read.csv(file[i]))
   }
-  mean(data[[pollutant]],na.rm = TRUE)
+  return (mean(data[[pollutant]],na.rm = TRUE))
+}
+
+complete<-function(directory, id) {
+  path<-paste("Desktop/RLabs", directory, sep = '/')
+  file<-list.files(path, full.names = TRUE)
+  data<-data.frame()
+  for(i in id) {
+    compl_sum<-sum(complete.cases(read.csv(file[i])))
+    data<-rbind(data, data.frame(i, compl_sum))
+  }
+  return (data)
 }
